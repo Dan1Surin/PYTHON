90342a3678e27f187e1be191c4e3bcf6398714dd
Dan1Surin <dan1surin@gmail.com>
2023-04-25 08:15:54 +0300

    добавил контрольные решения

--- HomeWork004/01/main.py
+++ HomeWork004/01/main.py
@@ -12,4 +12,24 @@ list_m = {random.randint(0,10) for _ in range(m)}
 print(list_n,list_m)
 print(*list_n & list_m)
 
+###решение из методички
 
+mol = [int(x) for x in input().split()]
+n = mol[0]
+m = mol[1]
+set_1 = set()
+set_2 = set()
+list_1 = list()
+a = [int(x) for x in input().split()]
+k = set(a)
+for i in k:
+    set_1.add(i)
+b = [int(x) for x in input().split()]
+k1 = set(b)
+for i in k1:
+    set_2.add(i)
+lok = set_1 & set_2
+kool = list(lok)
+kool.sort()
+for i in kool:
+    print(i, end=' ')
--- HomeWork004/02/main.py
+++ HomeWork004/02/main.py
@@ -9,3 +9,15 @@
 # непосредственно перед некоторым кустом, собирает ягоды с этого куста и с двух соседних с ним.
 # Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход
 # собирающий модуль, находясь перед некоторым кустом заданной во входном файле грядки.
+
+n=int(input('write n'))
+list_n=[]
+for i in range(n):
+    x = int(input('x'))
+    list_n.append(x)
+
+list_n_count=[]
+for i in range(len(list_n)-1):
+    list_n_count.append(list_n[i-1]+list_n[i]+list_n[i+1])
+list_n_count.append(list_n[-2]+list_n[-1]+list_n[0])
+print(max(list_n_count))
