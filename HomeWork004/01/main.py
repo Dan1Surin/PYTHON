d7c9b8b6cd1dfd2879b972acb750b57c516f5a78
Dan1Surin <dan1surin@gmail.com>
2023-04-25 08:02:55 +0300

    выполнил дз

--- HomeWork004/01/main.py
+++ HomeWork004/01/main.py
@@ -2,6 +2,14 @@
 # Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов
 # второго множества. Затем пользователь вводит сами элементы множеств.
+import random
 
+n=int(input('Write number of elements n = '))
+m=int(input('Write number of elements m = '))
+list_n = {random.randint(0,10) for _ in range(n)}
+list_m = {random.randint(0,10) for _ in range(m)}
+
+print(list_n,list_m)
+print(*list_n & list_m)
 
 
