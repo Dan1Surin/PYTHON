5a96154fb6cb56dfb44f12fb8666bcac690e4349
Dan1Surin <dan1surin@gmail.com>
2023-05-23 11:33:40 +0300

    Лкция по функциям высшего порядка

--- Lesson01/Lesson07/main.py
+++ Lesson01/Lesson07/main.py
@@ -1,7 +1,100 @@
 ### Функции высшего порядка
 
-list_1=[x for x in range(1,20)]
-print(list_1)
+## Функция в функции
 
-list_1=list(map(lambda x:x+10,list_1))
-print(list_1)
+def calk(a, b):
+    return a + a
+
+
+def math(op, x, y):
+    print(op(x, y))
+
+
+# math(lambda a,b:a+b,3,5)
+
+data = [1, 2, 3, 5, 8, 15, 23, 38]
+res = list()
+
+
+def simple(data, res):
+    for i in data:
+        if i % 2 == 0:
+            res.append((i, i ** 2))
+    print(res)
+
+
+# simple(data,res)
+
+## select возвращает элементы списка к которым применили функцию f(любую которуб передали при вызове)
+def select(f, col):
+    return [f(x) for x in col]  # возвращает список[]
+
+
+# возвращает только то что попадает под условия заданной функции f
+def where(f, col):
+    return [x for x in col if f(x)]
+
+
+res = select(int, data)
+# print(res)
+res = where(lambda x: x % 2 == 0, res)
+# print(res)
+res = list(select(lambda x: (x, x ** 2), res))
+# print(res)
+
+
+## функция map то же что мы делали в функции select
+# только в одну строку,
+# принимает значение и функцию которую надо применить к значению
+
+list_1 = [x for x in range(1, 20)]
+# print(list_1)
+
+list_1 = list(map(lambda x: x + 10, list_1))
+
+# print(list_1)
+
+# .split() преобразует строку в список строк(указывает разделитель)
+
+data1 = '15 34 23 656 78 8 9 0 544 3'
+
+# data1=data1.split()
+# print(data1)
+
+data1 = list(map(int, data1.split()))
+print(data1)
+
+## Функция фильтр принимает функцию и значение
+# (возвращает только то что при вызове функции вернет занчение true)
+
+data2 = [12, 34, 567, 15, 23, 175, 5, 231, 25]
+res1 = list(filter(lambda x: x % 10 == 5, data2))
+print(res1)
+
+# zip склеивает элементы на одной позиции из заданных списков выводя кортеж
+
+users=['us1','us2','us3']
+ids=[4,3,4,6,7]
+data11=list(zip(users,ids))
+print(data11)
+
+# enumerate выводит порядковый номер
+
+users=['us1','us2','us3']
+data11=list(enumerate(users))
+print(data11)
+
+
+# файлы
+
+# append - a
+# read - r
+# write - w
+# w+
+# r+
+
+colors=['red','green','blue']
+data22=open('file.txt','a')
+data22.write(colors)
+data22.close()
+
